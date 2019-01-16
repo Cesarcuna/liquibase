78e1c57423aea70d46ecec24c6c20ef0a55042e0
minisha <minisha.mit@gmail.com>
2019-01-16 22:47:11 +0800

    rest end point configuration

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -5,6 +5,8 @@ import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class UserService {
 
@@ -13,5 +15,13 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    public Optional<User> get(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public Optional<User> get(String firstName) {
+        return userRepository.findByFirstName(firstName);
     }
 }
